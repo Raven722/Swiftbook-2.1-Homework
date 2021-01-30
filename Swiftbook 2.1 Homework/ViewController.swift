9c6a4f2d7039b311bba248d3cabaa5eaef99b1dd
Agelyus <gelyusandrey@yandex.ru>
2021-01-30 23:56:48 +0500

    Initial Commit

---
+++ Swiftbook 2.1 Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook 2.1 Homework
+//
+//  Created by user on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
